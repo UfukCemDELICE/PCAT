1140add1eea0cf714e47eaac7ade0cfecac3a727
Ufuk Cem Delice <ufukcemdelice02@gmail.com>
2022-07-21 23:41:50 +0300

    partials, views, etc

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 const express = require('express');
-const app = express();
+const ejs = require('ejs');
 const path = require('path');
+const app = express();
+
+//Template Methot
+app.set('view engine', 'ejs');
+
 //MiddleWares
-app.use(express.static('public'))
+app.use(express.static('public'));
 
+//Routes
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'temp/index.html'))
+  res.render('index');
 });
-
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+app.get('/add', (req, res) => {
+  res.render('add');
+});
 const port = 3000;
 app.listen(port, () => {
   console.log(`Sunucu ${port} portunda başlatıldı...`);
