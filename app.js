d8cf54cf8e4f27d1292ab59f7cade333473daa4c
Ufuk Cem Delice <ufukcemdelice02@gmail.com>
2022-07-29 23:11:39 +0300

    db connections, photo page connections etc

--- app.js
+++ app.js
@@ -1,17 +1,37 @@
 const express = require('express');
 const ejs = require('ejs');
 const path = require('path');
+const Photo = require('./models/Photo');
+const mongoose = require('mongoose');
 const app = express();
+// connect DB
+mongoose.connect('mongodb://localhost/pcat-test-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 //Template Methot
 app.set('view engine', 'ejs');
 
 //MiddleWares
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 //Routes
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', async (req, res) => {
+  const photos = await Photo.find({});
+  res.render('index', {
+    photos,
+  });
+});
+app.get('/photos/:id', async(req, res) => {
+  //console.log(req.params.id)
+  //res.render('about');
+  const photo = await Photo.findById(req.params.id);
+  res.render('photo', {
+    photo
+  })
 });
 app.get('/about', (req, res) => {
   res.render('about');
@@ -19,6 +39,11 @@ app.get('/about', (req, res) => {
 app.get('/add', (req, res) => {
   res.render('add');
 });
+app.post('/photos', async (req, res) => {
+  await Photo.create(req.body);
+  res.redirect('/');
+});
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`Sunucu ${port} portunda başlatıldı...`);
