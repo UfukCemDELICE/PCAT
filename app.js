66ce1eba5ed4a14d69e66e5ca53099bc27978169
Ufuk Cem Delice <ufukcemdelice02@gmail.com>
2022-08-01 01:42:51 +0300

    Ready for deployment

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const express = require('express');
-const ejs = require('ejs');
-const path = require('path');
-const Photo = require('./models/Photo');
 const mongoose = require('mongoose');
+const fileUpload = require('express-fileupload');
+const methodOverride = require('method-override');
+const ejs = require('ejs');
+const photoController = require('./controllers/photoControllers');
+const pageController = require('./controllers/pageController');
+
 const app = express();
 // connect DB
-mongoose.connect('mongodb://localhost/pcat-test-db', {
+mongoose.connect('mongodb+srv://ufukcemdelice:Ufukcem1998.@cluster0.psbpq.mongodb.net/pcat-db?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-});
+}).then(() => {
+  console.log('DB CONNECTED')
+}).catch((err) => {
+  console.log(err)
+})
 
 //Template Methot
 app.set('view engine', 'ejs');
@@ -17,34 +24,24 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(fileUpload());
+app.use(methodOverride('_method', {
+  methods: ['POST', 'GET']
+}));
 
 //Routes
-app.get('/', async (req, res) => {
-  const photos = await Photo.find({});
-  res.render('index', {
-    photos,
-  });
-});
-app.get('/photos/:id', async(req, res) => {
-  //console.log(req.params.id)
-  //res.render('about');
-  const photo = await Photo.findById(req.params.id);
-  res.render('photo', {
-    photo
-  })
-});
-app.get('/about', (req, res) => {
-  res.render('about');
-});
-app.get('/add', (req, res) => {
-  res.render('add');
-});
-app.post('/photos', async (req, res) => {
-  await Photo.create(req.body);
-  res.redirect('/');
-});
+app.get('/', photoController.getAllPhotos);
+app.get('/photos/:id', photoController.getPhoto);
+app.post('/photos', photoController.createPhoto);
+app.put('/photos/:id', photoController.updatePhoto);
+app.delete('/photos/:id', photoController.deletePhoto);
+app.get('/about', pageController.getAboutPage);
+app.get('/add', pageController.getAddPage);
+app.get('/photos/edit/:id', pageController.getEditPage);
 
-const port = 3000;
+
+
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Sunucu ${port} portunda başlatıldı...`);
 });
